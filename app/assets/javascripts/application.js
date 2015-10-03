1a99b481b68a56e7969e5dc2313793ede9829f6d
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-10-03 18:37:08 -0300

    angular with static data

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require angular/angular
-//= require_tree .
+//= require angular
+//= require angular-route/angular-route
+//= require angular-rails-templates
+//= require app
+//= require_tree ./templates
