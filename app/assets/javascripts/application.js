a8ede2c0065dad3ee13a6e26f6227f603acbf757
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-10-03 23:02:39 -0300

    show recipe

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require angular
+//= require angular/angular
 //= require angular-route/angular-route
+//= require angular-resource/angular-resource
 //= require angular-rails-templates
-//= require app
-//= require_tree ./templates
+//= require angular-flash/dist/angular-flash
+//= require_tree .
